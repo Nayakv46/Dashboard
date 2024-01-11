8187d1af4c3649bf61e41a6ea69cd005efcc8c55
Nayakv46 <bartoszjaniak04@gmail.com>
2024-01-11 15:56:47 +0100

    responsiveness of accordeon items (cards)

--- src/components/AccordeonItem/AccordeonItem.jsx
+++ src/components/AccordeonItem/AccordeonItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 import './accordeonItem.scss';
 
@@ -8,14 +8,38 @@ import { IoLogoGameControllerB } from "react-icons/io";
 
 const AccordeonItem = ({openedItem, setOpenedItem, index, image, name, shortName}) => {
 
+
+    // A max value for window.innerWidth between (540, 991)
+    const [maxHeight, setMaxHeight] = useState(`280px`);
+
+    // On window resize, check if the state of maxHeight is supposed to be updated
+    window.addEventListener('resize', () => {
+        const windowWidth = window.innerWidth;
+        if(windowWidth <= 540){
+            // set the state for a variable passed to css
+            // windowWidth - padding * 280/500 ratio
+            setMaxHeight(`${(windowWidth - 40) * 0.56}px`);
+        }
+    })
+
+    // Run function once on load to check if the window is smaller than 540 and update the state if so
+    useEffect(() => {
+        const windowWidth = window.innerWidth;
+        if(windowWidth <= 540){
+            setMaxHeight(`${(windowWidth - 40) * 0.56}px`);
+        }
+    }, []);
+
     return (
     <div
         className={`accordeonItem ${openedItem === index ? 'accordeonItem--opened' : ''}`}
+        style={{"--maxHeight": maxHeight}}
         onClick={() => {
             setOpenedItem(index)
         }}
     >
-        <img className='accordeonItem__image' src={image} alt="poster" />
+        <div className='accordeonItem__image' style={{backgroundImage: `url('${image}')`}}>
+        </div>
 
         <div className='accordeonItem__overlay'>
 
@@ -34,7 +58,7 @@ const AccordeonItem = ({openedItem, setOpenedItem, index, image, name, shortName
 
                 <span className='accordeonItem__title'>
                     <span className='accordeonItem__title-text'>
-                        {name}
+                        {window.innerWidth > 400 ? `${name}` : `${shortName}`}
                     </span>
                 </span>
             </div>
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './statistics.scss';
+
+const Statistics = () => {
+  return (
+    <div>Statistics</div>
+  )
+}
+
+export default Statistics
